21f2d2b8a87ce5f713df53613b120ffdc53bcaf8
javier@hodlgroup.com <javier@hodl.nl>
2026-02-23 11:46:08 +0100

    feat: Editor css created for gutenberg styling

--- functions.php
+++ functions.php
@@ -193,3 +193,8 @@ function bespokerx_register_taxonomy() {
     ]);
 }
 add_action( 'init', 'bespokerx_register_taxonomy' );
+
+add_action('after_setup_theme', function () {
+  add_theme_support('editor-styles');
+  add_editor_style('assets/css/editor.css');
+});
